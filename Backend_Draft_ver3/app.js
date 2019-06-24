317d5c4be48a551899f18d667cc5e3f1221b7df5
rufomarkjorge <52167137+rufomarkjorge@users.noreply.github.com>
2019-06-24 23:17:20 +0800

    Node Insurance ver 3
    with chatbot service

---
+++ Backend_Draft_ver3/app.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+// store config variables in dotenv
+require('dotenv').config();
+const cors = require('cors');
+
+// ****** allow cross-origin requests code START ****** //
+app.use(cors()); // uncomment this to enable all CORS and delete cors(corsOptions) in below code
+const allowedOrigins = process.env.allowedOrigins.split(',');
+/**
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+ */
+// ****** allow cross-origin requests code END ****** //
+
+// ****** validation rules START ****** //
+const valFunctions = require('./validators/validate');
+// ****** validation rules END ****** //
+
+
+// app Routes
+// create application/json parser
+const jsonParser = bodyParser.json()
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// POST /login gets urlencoded bodies
+app.post('/login', jsonParser, function (req, res) {
+    if(valFunctions.checkInputDataNULL(req,res)) return false;
+    if(valFunctions.checkInputDataQuality(req,res)) return false;
+
+    var dbFunctions = require('./models/connector');
+    //dbFunctions.loginUser(req,res);
+    dbFunctions.loginUser(req,res);
+});
+app.get('/profile', jsonParser, function (req, res) {
+    if(valFunctions.checkInputDataNULL(req,res)) return false;
+    if(valFunctions.checkInputDataQuality(req,res)) return false;
+    if(valFunctions.checkJWTToken(req,res)) return false;
+    //if(valFunctions.checkUserAuthRole(req,res)) return false;
+    var dbFunctions = require('./models/connector');
+    dbFunctions.getUser(req,res);
+});
+app.post('/profile', jsonParser, function (req, res) {
+    if(valFunctions.checkInputDataNULL(req,res)) return false;
+    if(valFunctions.checkInputDataQuality(req,res)) return false;
+    if(valFunctions.checkJWTToken(req,res)) return false;
+    //if(valFunctions.checkUserAuthRole(req,res)) return false;
+    var dbFunctions = require('./models/connector');
+    dbFunctions.updateUser(req,res);
+});
+app.get('/allpolicies', jsonParser, function (req, res) {
+    if(valFunctions.checkInputDataNULL(req,res)) return false;
+    if(valFunctions.checkInputDataQuality(req,res)) return false;
+    if(valFunctions.checkJWTToken(req,res)) return false;
+    //if(valFunctions.checkUserAuthRole(req,res)) return false;
+    var dbFunctions = require('./models/connector');
+    dbFunctions.getPolicy(req,res);
+});
+app.post('/bot', jsonParser, function (req, res) {
+    console.log(req.query.request);
+    var dbFunctions = require('./models/connector');
+    dbFunctions.pushChat(req,res);
+});
+
+app.use('/', (req, res) => res.send("Welcome!"));
+app.listen(process.env.PORT, () => console.log('Server is ready on localhost:' + process.env.PORT));
---
+++ Backend_Draft_ver3/models/connector.js
@@ -0,0 +1,221 @@
+const pool = require('./dbconnection');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
+var resultsNotFound = {
+  "errorCode": "0",
+  "errorMessage": "Operation not successful.",
+  "rowCount": "0",
+  "data": ""
+};
+var resultsFound = {
+  "errorCode": "1",
+  "errorMessage": "Operation successful.",
+  "rowCount": "1",
+  "data": ""
+};
+
+var tokenUpdated = {
+  "message" : "Token Updated"
+};
+
+var AWS = require('aws-sdk');
+		// Initialize the Amazon Cognito credentials provider
+		AWS.config.region = process.env.region; // Region
+		AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+		IdentityPoolId: process.env.IdentityPoolId,});
+		// Initialize the Amazon Cognito credentials provider
+		//AWS.config.region = 'us-east-1'; // Region
+		//AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+		// Provide your Pool Id here
+		//	IdentityPoolId: 'us-east-1:XXXXX',
+		//});
+
+		var lexruntime = new AWS.LexRuntime();
+		var lexUserId = 'chatbot-demo' + Date.now();
+		var sessionAttributes = {};
+
+module.exports = {
+//CREATE USER DRAFT
+//   createUser: function (req, res) {
+//     pool.getConnection(function (err, connection) {
+//       if (err) throw err; // not connected!
+
+//       bcrypt.hash(req.body.inputPassword, saltRounds, function (err, hash) {
+//         //var sql = 'INSERT INTO user SET ?';
+//         //var values = { 'name': req.body.inputEmail, 'email': req.body.inputEmail, 'password': hash, 'createdAt': new Date(), 'updatedAt': new Date() }
+//         // Use the connection
+//         //TESTInsertdata//
+//         var sql = 'INSERT INTO login SET ?';
+//         var values = { 'userid': '11111', 'username': req.body.inputEmail, 'password': hash, 'role': 'Admin', 'accountno': '12341234' }
+//         //TESTInsertdata//
+//         connection.query(sql, values, function (error, results, fields) {
+//           if (error) {
+//             resultsNotFound["errorMessage"] = "emailID already exists.";
+//             return res.send(resultsNotFound);
+//           } else return res.send(resultsFound);
+
+//           // When done with the connection, release it.
+//           connection.release(); // Handle error after the release.
+//           if (error) throw error; // Don't use the connection here, it has been returned to the pool.
+//         });
+//       });
+//     });
+// },
+  loginUser: function (req, res) {
+    pool.getConnection(function (err, connection) {
+      if (err) throw err; // not connected!
+
+        var sql = 'SELECT * FROM `login` WHERE `username` = ?';
+        var values = [req.body.inputEmail]
+        // Use the connection
+        connection.query(sql, values, function (error, results, fields) {
+          if (error) {
+            resultsNotFound["errorMessage"] = "Something went wrong with Server.";
+            return res.send(resultsNotFound);
+          }
+          if (results =="") {
+            resultsNotFound["errorMessage"] = "User Id not found.";
+            return res.send(resultsNotFound);
+          }
+          bcrypt.compare(req.body.inputPassword, results[0].password, function (err, result) {
+      if (result == true) {
+        var token = {
+          "token": jwt.sign(
+            { email: req.body.inputEmail },
+            process.env.JWT_SECRET,
+            { expiresIn: "30d" }
+          )  
+        }
+        resultsFound["userid"] = results[0].userid;
+        resultsFound["data"] = token;
+        //res.send(resultsFound);
+        //START*** to Insert token to Database Table login
+        var sqltoken = 'UPDATE login SET ? WHERE `username` = ?';
+        var userEmail = req.body.inputEmail;
+        Date.prototype.addHours = function(h){
+          this.setHours(this.getHours()+h);
+          return this;
+        }
+        var valuestoken = { 'session': token["token"],'session_timeout': new Date().addHours(1)};
+        connection.query(sqltoken, [valuestoken,userEmail], function (error, resultstoken, fields){
+            res.send(resultsFound);
+        });
+        //END*** to Insert token to Database Table login
+      } else {
+        resultsNotFound["errorMessage"] = "Incorrect Password.";
+        return res.send(resultsNotFound);
+      }
+    });
+
+          // When done with the connection, release it.
+          connection.release(); // Handle error after the release.
+          if (error) throw error; // Don't use the connection here, it has been returned to the pool.
+        });
+      });
+  },
+  getUser: function (req, res) {
+    pool.getConnection(function (err, connection) {
+      if (err) throw err; // not connected!
+
+        var sql = 'SELECT * FROM user_profile JOIN tblpolicy ON user_profile.userid = tblpolicy.userid WHERE user_profile.userid = ?';
+        var values = [req.body.userid]
+        console.log(req.body.userid);
+        // Use the connection
+        connection.query(sql, values, function (error, results, fields) {
+          if (error) {
+            resultsNotFound["errorMessage"] = "Something went wrong with Server.";
+            return res.send(resultsNotFound);
+          }
+          if (results =="") {
+            resultsNotFound["errorMessage"] = "User Id not found.";
+            return res.send(resultsNotFound);
+          }
+          resultsFound["data"] = results;
+          res.send(resultsFound);
+          console.log(results);
+          // When done with the connection, release it.
+          connection.release(); // Handle error after the release.
+          if (error) throw error; // Don't use the connection here, it has been returned to the pool.
+        });
+      });
+  },
+  updateUser: function (req, res) {
+    pool.getConnection(function (err, connection) {
+      if (err) throw err; // not connected!
+
+        var sql = 'UPDATE user_profile SET ? WHERE userid = ?';
+        var userid = [req.body.userid];
+        console.log(req.body.userid);
+        //ADD ALL FIELDS ON var userdetails
+        var userdetails = {'fname': req.body.fname, 'lname': req.body.lname, 'mname': req.body.mname};
+        // Use the connection
+        connection.query(sql, [userdetails,userid], function (error, results, fields) {
+          if (error) {
+            resultsNotFound["errorMessage"] = "Something went wrong with Server.";
+            return res.send(resultsNotFound);
+          }
+          if (results =="") {
+            resultsNotFound["errorMessage"] = "User Id not found.";
+            return res.send(resultsNotFound);
+          }
+          resultsFound["errorMessage"] = "User Details Updated";
+          res.send(resultsFound);
+          console.log(results);
+          // When done with the connection, release it.
+          connection.release(); // Handle error after the release.
+          if (error) throw error; // Don't use the connection here, it has been returned to the pool.
+        });
+      });
+  },
+  getPolicy: function (req, res) {
+    pool.getConnection(function (err, connection) {
+      if (err) throw err; // not connected!
+
+        var sql = 'SELECT * FROM `tblpolicy` WHERE `userid` = ?';
+        var values = [req.body.userid]
+        console.log(req.body.userid);
+        // Use the connection
+        connection.query(sql, values, function (error, results, fields) {
+          if (error) {
+            resultsNotFound["errorMessage"] = "Something went wrong with Server.";
+            return res.send(resultsNotFound);
+          }
+          if (results =="") {
+            resultsNotFound["errorMessage"] = "User Id not found.";
+            return res.send(resultsNotFound);
+          }
+          resultsFound["data"] = results;
+          res.send(resultsFound);
+          console.log(results);
+          // When done with the connection, release it.
+          connection.release(); // Handle error after the release.
+          if (error) throw error; // Don't use the connection here, it has been returned to the pool.
+        });
+      });
+  },
+  //CHATBOT-ver1
+  pushChat: function pushChat(req, res) {
+		console.log("PUSHCHAT",req.query.request);
+		var params = {
+			botAlias: process.env.botAlias,
+			botName: process.env.botName,
+			inputText: req.query.request,
+			userId: lexUserId,
+			sessionAttributes: sessionAttributes
+		};
+		lexruntime.postText(params, function(err, data) {
+			if (err) {
+				console.log(err, err.stack);
+				showError('Error:  ' + err.message + ' (see console for details)')
+			}
+			if (data) {
+			sessionAttributes = data.sessionAttributes;
+				console.log("AMAZONLEX",data);
+				return res.send(data);
+			}
+		});
+		return false;
+	}
+};
